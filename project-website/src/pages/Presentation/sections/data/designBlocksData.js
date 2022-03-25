e961364a5b95868e3f9b2f8652b888822df21ed5
daniff15 <dani.fig@ua.pt>
2022-03-25 23:43:32 +0000

    asd

--- project-website/src/pages/Presentation/sections/data/designBlocksData.js
+++ project-website/src/pages/Presentation/sections/data/designBlocksData.js
@@ -13,9 +13,6 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-const imagesPrefix =
-  "https://raw.githubusercontent.com/creativetimofficial/public-assets/master/material-design-system/presentation/sections";
-
 export default [
   {
     title: "Equipa",
