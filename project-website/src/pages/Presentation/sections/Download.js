f219307790e1c7979bd82f24a743ee8c47834e76
daniff15 <dani.fig@ua.pt>
2022-03-25 23:43:17 +0000

    toques

--- project-website/src/pages/Presentation/sections/Download.js
+++ project-website/src/pages/Presentation/sections/Download.js
@@ -14,63 +14,12 @@ Coded by www.creative-tim.com
 */
 
 // @mui material components
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
-import MKButton from "components/MKButton";
-import MKTypography from "components/MKTypography";
 
 // Images
-import bgImage from "assets/images/shapes/waves-white.svg";
-
 function Download() {
-  return (
-    <MKBox component="section" py={{ xs: 0, sm: 12 }}>
-      <MKBox
-        variant="gradient"
-        bgColor="dark"
-        position="relative"
-        borderRadius="xxl"
-        sx={{ overflow: "hidden" }}
-        height={{ xs: "10vh", sm: "40vh", md: "40vh", lg: "30vh" }}
-      >
-        <MKBox
-          component="img"
-          src={bgImage}
-          alt="pattern-lines"
-          position="absolute"
-          top={0}
-          left={0}
-          width="100%"
-          zIndex={1}
-          opacity={0.3}
-        />
-        <Container sx={{ position: "relative", zIndex: 2, py: 12 }}>
-          <Grid container item xs={12} md={7} justifyContent="center" mx="auto" textAlign="center">
-            <MKTypography variant="h3" color="white">
-              Do you want to see more?
-            </MKTypography>
-            <MKTypography variant="body2" color="white" mb={6}>
-              Check it out on our Youtube Channel, and stay knowing more about us and our project.
-            </MKTypography>
-
-            <MKButton
-              variant="gradient"
-              color="info"
-              size="large"
-              component="a"
-              href="https://www.creative-tim.com/product/material-kit-react"
-              sx={{ mb: 2 }}
-            >
-              DEMO (por aqui o simbolo do youtobas)
-            </MKButton>
-          </Grid>
-        </Container>
-      </MKBox>
-    </MKBox>
-  );
+  return <MKBox component="section" py={{ xs: 0, sm: 12 }} />;
 }
 
 export default Download;
