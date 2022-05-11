0f86c7185c8012241902bb2a11c1933b265b7a99
daniff15 <dani.fig@ua.pt>
2022-05-11 15:09:04 +0100

    functional button routing

--- Viewers-master/platform/viewer/src/components/Admin/Dashboard/nestedComponents/Sidebar.js
+++ Viewers-master/platform/viewer/src/components/Admin/Dashboard/nestedComponents/Sidebar.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './Sidebar.css';
 
+import { useHistory } from 'react-router-dom';
+
 function Sidebar() {
+  const history = useHistory();
+
+  const initDashboard = () => {
+    history.push('/');
+  };
+
+  const users = () => {
+    history.push('/users');
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
           <div className="list">
             <div>
-              <li className="listElem">Dashboard</li>
-              <li className="listElem">Manage Acess</li>
+              <li className="listElem" onClick={initDashboard}>
+                Dashboard
+              </li>
+              <li className="listElem" onClick={users}>
+                Manage Acess
+              </li>
               <li className="listElem">SW Versions</li>
               <li className="listElem">DICOM Nodes</li>
             </div>
