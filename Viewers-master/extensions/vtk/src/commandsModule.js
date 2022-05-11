de7b06186e0a0df3766417eb0d4d26ed3bd0c6c6
eva-pomposo <evabartolomeu@ua.pt>
2022-05-11 19:24:09 +0100

    try fix 3D

--- Viewers-master/extensions/vtk/src/commandsModule.js
+++ Viewers-master/extensions/vtk/src/commandsModule.js
@@ -508,6 +508,8 @@ const commandsModule = ({ commandsManager, servicesManager }) => {
         document.getElementsByClassName('vtk-viewport-handler')
       );
 
+      /*
+
       const url = 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs';
       const studyInstanceUID =
         '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463';
@@ -519,6 +521,9 @@ const commandsModule = ({ commandsManager, servicesManager }) => {
         url,
         searchInstanceOptions
       );
+      */
+
+      vistaActivada[0] = VTKVolumeRenderingExample.componentDidMount();
       
       //vistaActivada[0].innerHTML = VTKVolumeRenderingExample.VTKFusionExample.render();
       //ReactDOM.render(VTKVolumeRenderingExample, vistaActivada[0]);
