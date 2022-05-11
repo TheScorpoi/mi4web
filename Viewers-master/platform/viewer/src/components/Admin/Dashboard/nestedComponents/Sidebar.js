ff4494dd68838d84cf9819ec838d8a1e1fc6ae8c
MartaFradique <martafradique@ua.pt>
2022-05-11 15:50:04 +0100

    side bar resising with width

--- Viewers-master/platform/viewer/src/components/Admin/Dashboard/nestedComponents/Sidebar.js
+++ Viewers-master/platform/viewer/src/components/Admin/Dashboard/nestedComponents/Sidebar.js
@@ -3,18 +3,20 @@ import './Sidebar.css';
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <div className="sidebarWrapper">
-        <div className="sidebarMenu">
+    <div className="sidebarWrapper">
+      <div className="sidebarMenu">
+        <div className="sidebar">
           <div className="list">
-            <div>
-              <li className="listElem">Dashboard</li>
-              <li className="listElem">Manage Acess</li>
-              <li className="listElem">SW Versions</li>
-              <li className="listElem">DICOM Nodes</li>
-            </div>
-            <div>
-              <li className="listElem2 ">Extensions Installation</li>
+            <div id="sidebarDeasapear">
+              <div>
+                <li className="listElem">Dashboard</li>
+                <li className="listElem">Manage Acess</li>
+                <li className="listElem">SW Versions</li>
+                <li className="listElem">DICOM Nodes</li>
+              </div>
+              <div>
+                <li className="listElem2 ">Extensions Installation</li>
+              </div>
             </div>
           </div>
         </div>
