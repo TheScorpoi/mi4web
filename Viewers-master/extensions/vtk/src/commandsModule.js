c55d11f4dfbd36690b740814fb3a028fd06faa5a
eva-pomposo <evabartolomeu@ua.pt>
2022-05-13 01:34:14 +0100

    fix toolbar 3D

--- Viewers-master/extensions/vtk/src/commandsModule.js
+++ Viewers-master/extensions/vtk/src/commandsModule.js
@@ -510,8 +510,23 @@ const commandsModule = ({ commandsManager, servicesManager }) => {
         document.getElementsByClassName('vtk-viewport-handler')
       );
 
-      ReactDOM.render(<VTKFusionExample />, vistaActivada[0]);
+      const toolBar = Array.from(document.getElementsByClassName('ToolbarRow'));
 
+      for (let index = 2; index < 7; index++) {
+        let element = toolBar[0].children[index];
+        element.style.visibility = 'hidden';
+      }
+
+      toolBar[0].children[1].children[1].innerText = 'Exit 3D';
+
+      let view = document.getElementsByClassName(
+        'viewport-drop-target viewport-container active'
+      );  
+
+      view[0].children[0].style.visibility = 'hidden';
+      view[0].children[1].style.visibility = 'hidden';
+
+      ReactDOM.render(<VTKFusionExample />, vistaActivada[0]);
     },
   };
 
