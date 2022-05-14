18690619c531f5a794ce8f5a0cdbabd61daa1b3f
daniff15 <dani.fig@ua.pt>
2022-05-14 20:05:38 +0100

    login and register

--- authentication/uap/uap.py
+++ authentication/uap/uap.py
@@ -16,17 +16,19 @@ import json as JSON
 import base64
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+
 def generate_key_uap():
     '''Generates a key to encrypt and decrypt JSON files'''
     if not os.path.isfile('filekey.key'):
-        key = Fernet.generate_key() # chave que vai encriptar a password e o salt
+        key = Fernet.generate_key()  # chave que vai encriptar a password e o salt
         with open('filekey.key', 'wb') as f:
             f.write(key)
             return key
     else:
-        with open ('filekey.key', 'rb') as f:
+        with open('filekey.key', 'rb') as f:
             key = f.read()
             return key
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -38,9 +40,10 @@ salt = ""
 logged_in = False
 
 
-server_hello ='http://localhost:5000/send_hello'
+server_hello = 'http://localhost:5000/send_hello'
 server_challenge = ''
 server_done = ''
+
 
 @app.route('/show_login', methods=['GET'])
 def show_login():
@@ -56,18 +59,20 @@ def show_login():
             salt=salt,
             iterations=390000,
         )
-        key_bd = base64.urlsafe_b64encode(kdf.derive(password_key.encode("utf-8")))
+        key_bd = base64.urlsafe_b64encode(
+            kdf.derive(password_key.encode("utf-8")))
 
-        # desincriptar o ficheiro            
+        # desincriptar o ficheiro
         f = Fernet(key_bd)
         content = f.decrypt(bd_file.read())
 
         bd = JSON.loads(content.decode("utf-8"))
         bd_file.close()
 
-        if page1 in bd.keys():           
+        if page1 in bd.keys():
             return render_template('login.html', page=page1, bd=bd[page1], logged_in=logged_in)
     return render_template('login.html', bd=[], logged_in=logged_in)
+
 
 @app.route('/confirm_password', methods=['POST', 'GET'])
 def confirm_password():
@@ -85,6 +90,12 @@ def confirm_password():
             return redirect(url_for('show_login'))
     else:
         return render_template('confirm_password.html', password="")
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    return render_template('register.html', password="")
+
 
 @app.route('/update_password', methods=['POST', 'GET'])
 def update_password():
@@ -115,12 +126,13 @@ def update_password():
                 salt=salt,
                 iterations=390000,
             )
-            key_bd = base64.urlsafe_b64encode(kdf.derive(password_key.encode("utf-8")))
+            key_bd = base64.urlsafe_b64encode(
+                kdf.derive(password_key.encode("utf-8")))
 
             # carregar o ficheiro encriptado
             bd_file = open("bd.json", "rb")
 
-            # desincriptar o ficheiro            
+            # desincriptar o ficheiro
             f = Fernet(key_bd)
             content = f.decrypt(bd_file.read())
 
@@ -128,7 +140,8 @@ def update_password():
             bd_file.close()
 
         # guardar a nova pass
-        to_store = JSON.dumps({'password': new_password}, indent=4, sort_keys=True).encode('utf-8')
+        to_store = JSON.dumps({'password': new_password},
+                              indent=4, sort_keys=True).encode('utf-8')
 
         f = Fernet(key)
         content = f.encrypt(to_store)
@@ -148,7 +161,8 @@ def update_password():
                 salt=salt,
                 iterations=390000,
             )
-            key_bd = base64.urlsafe_b64encode(kdf.derive(new_password.encode("utf-8")))
+            key_bd = base64.urlsafe_b64encode(
+                kdf.derive(new_password.encode("utf-8")))
             bd_file = open("bd.json", "wb")
             f = Fernet(key_bd)
             content = JSON.dumps(bd, indent=4, sort_keys=True).encode('utf-8')
@@ -160,6 +174,7 @@ def update_password():
     else:
         return render_template('update_password.html')
 
+
 @app.route('/store_password', methods=['POST'])
 def store_password():
     global key
@@ -169,9 +184,11 @@ def store_password():
     if len(password) == 0:
         return render_template('create_password.html', password="", error="A password não pode estar vazia")
 
-    _salt = os.urandom(16) # o salt só é calculado uma vez e depois tem de ser sempre o mesmo
+    # o salt só é calculado uma vez e depois tem de ser sempre o mesmo
+    _salt = os.urandom(16)
 
-    to_store = JSON.dumps({'password': password}, indent=4, sort_keys=True).encode('utf-8')
+    to_store = JSON.dumps({'password': password}, indent=4,
+                          sort_keys=True).encode('utf-8')
 
     # encriptar o ficheiro
     f = Fernet(key)
@@ -189,9 +206,10 @@ def store_password():
     password_key = password
     salt = _salt
 
-    logged_in = True # acabou de criar a password, fica logado
+    logged_in = True  # acabou de criar a password, fica logado
 
     return redirect(url_for('show_login'))
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -211,9 +229,9 @@ def home():
         content = f.decrypt(pass_file.read())
 
         x = JSON.loads(content.decode("utf-8"))
-        
+
         password_key = x["password"]
-        
+
         pass_file.close()
 
         salt_file = open("salt.txt", "rb")
@@ -224,8 +242,10 @@ def home():
         return redirect(url_for('confirm_password'))
     else:
         return render_template('create_password.html', password="")
-        
+
 #! mudar o nome da funcao
+
+
 @app.route('/get_informations', methods=['POST'])
 def get_informations():
     """Vai buscar os valores dos campos da pagina de autenticação, e faz os pedidos a API
@@ -247,12 +267,13 @@ def get_informations():
             salt=salt,
             iterations=390000,
         )
-        key_bd = base64.urlsafe_b64encode(kdf.derive(password_key.encode("utf-8")))
+        key_bd = base64.urlsafe_b64encode(
+            kdf.derive(password_key.encode("utf-8")))
 
         # carregar o ficheiro encriptado
         bd_file = open("bd.json", "rb")
 
-        # desincriptar o ficheiro            
+        # desincriptar o ficheiro
         f = Fernet(key_bd)
         content = f.decrypt(bd_file.read())
 
@@ -270,7 +291,7 @@ def get_informations():
             edit = False
             counter = 0
             for obj in bd[page1]:
-                if obj["username"] == user: # este user name existe na bd
+                if obj["username"] == user:  # este user name existe na bd
                     edit = True
                     break
                 counter += 1
@@ -278,11 +299,12 @@ def get_informations():
             if edit:
                 # aqui temos de atualizar
                 bd[page1][counter] = credentials
-            else:            
+            else:
                 bd[page1].append(credentials)
 
             bd_file = open("bd.json", "wb")
-            content = JSON.dumps(bd, indent=4, sort_keys=True).encode('utf-8') # para ler temos de fazer decode
+            content = JSON.dumps(bd, indent=4, sort_keys=True).encode(
+                'utf-8')  # para ler temos de fazer decode
 
             # encriptar o ficheiro
             kdf = PBKDF2HMAC(
@@ -291,7 +313,8 @@ def get_informations():
                 salt=salt,
                 iterations=390000,
             )
-            key_bd = base64.urlsafe_b64encode(kdf.derive(password_key.encode("utf-8")))
+            key_bd = base64.urlsafe_b64encode(
+                kdf.derive(password_key.encode("utf-8")))
             f = Fernet(key_bd)
             content = f.encrypt(content)
 
@@ -299,47 +322,53 @@ def get_informations():
             bd_file.close()
 
         else:
-            return render_template('login.html', page = page1, error="Tem de inserir a password para poder guardar credenciais", logged_in=logged_in)
+            return render_template('login.html', page=page1, error="Tem de inserir a password para poder guardar credenciais", logged_in=logged_in)
 
     password = password.encode("utf-8")
 
     digest = hashlib.sha3_512()
     digest.update(password)
     dict[user] = digest.hexdigest()
-    #mensagem de hello para o servidor
+    # mensagem de hello para o servidor
     retorno, challenge, token_tmp = send_hello(user)
     if retorno:
-        result, server_token_tmp = calculate_challenge(user, challenge, token_tmp)
+        result, server_token_tmp = calculate_challenge(
+            user, challenge, token_tmp)
         if result and valores_errados == False:
             return redirect(server_done+'?token_tmp='+server_token_tmp)
         elif valores_errados == True and result:
             #!voltar para a pagina de login, e dizer que "ahh oh servidor está a dizer que podes entrar, mas é ficticio"
             serverFicticio = True
             valores_errados = False
-            return render_template('login.html', page = page1, serverFicticio=serverFicticio, bd=bd[page1], logged_in=logged_in) # fazer warning a dizer que o user não existe no server
+            # fazer warning a dizer que o user não existe no server
+            return render_template('login.html', page=page1, serverFicticio=serverFicticio, bd=bd[page1], logged_in=logged_in)
         else:
             #!este é para quando a password esta mal
             serverFicticio = False
             incorrectPassword = True
             valores_errados = False
-            return render_template('login.html', page = page1, incorrectPassword=incorrectPassword, bd=bd[page1], logged_in=logged_in) # fazer warning a dizer que o user não existe no server  
+            # fazer warning a dizer que o user não existe no server
+            return render_template('login.html', page=page1, incorrectPassword=incorrectPassword, bd=bd[page1], logged_in=logged_in)
     else:
         userNotFound = True
-        return render_template('login.html', page = page1, userNotFound=userNotFound, bd=bd[page1], logged_in=logged_in) # fazer warning a dizer que o user não existe no server
-    
+        # fazer warning a dizer que o user não existe no server
+        return render_template('login.html', page=page1, userNotFound=userNotFound, bd=bd[page1], logged_in=logged_in)
+
     # if true -> continua a falar c ele e começa a fazer os desafios
     # if false -> retorna-mos ao login e dizeos username invalido
 
     return make_response(dict[user])
 
+
 @app.route('/send_hello', methods=['POST'])
 def send_hello(user):
     global server_hello, server_challenge
     """Envia uma mensagem para se dar a conhecer ao servidor, enviando-lhe o username de quem se quer autenticar"""
-    uap_url = request.url_root[0:-1] + url_for('calculate_challenge') #  [0:-1] para retirar a barra (/)
+    uap_url = request.url_root[0:-1] + \
+        url_for('calculate_challenge')  # [0:-1] para retirar a barra (/)
     # uap_url é o link para onde o servidor terá de responder
 
-    dict_ = {"username" : user}
+    dict_ = {"username": user}
     _request = requests.post(server_hello, data=dict_)
     _request = _request.json()
     if 'error' in _request:
@@ -353,16 +382,18 @@ def send_hello(user):
         return True, challenge, token_tmp
 
 #! estamos so a fazer para um user, no entanto, o server pode estar a comunicar com mais que um
-#! user ao memso tempo, depois pensar numa maneira de levar o user na mensagem para o server saber 
+#! user ao memso tempo, depois pensar numa maneira de levar o user na mensagem para o server saber
 #! com quem esta a falar
+
+
 @app.route('/calculating_challenge', methods=['POST'])
 def calculate_challenge(user, challenge, token_tmp):
     """Verifica se o bit recebido corresponde ao que ele pretende, fazendo isto tantas vezes quanto o tamanho da mensagem.
     Caso seja verdadade, calcula uma nova mensagem com a password, o proximo challenge dele, e o bit do resultado do dele.
     Caso seja Falso, ele apenas envia o challenge e um bit aleatório.
     O UAP apercebe-se que chegou ao fim, quando recebe uma mensagem 'finnish', e acaba de trocar challenges com o servidor"""
-    
-    global dict, valores_errados,server_challenge, server_done
+
+    global dict, valores_errados, server_challenge, server_done
     password = dict[user]
     rnd = randint(0, 100)
     message = password + str(challenge) + str(rnd)
@@ -371,17 +402,17 @@ def calculate_challenge(user, challenge, token_tmp):
     bits = BitArray(hex=digest.hexdigest())
     bit_to_send = bits.bin[0]
     dict = {"challenge": rnd, "bit": bit_to_send, "token_tmp": token_tmp}
-        
+
     challenge = rnd
-    
-    while True:    
-        _request =  requests.post(server_challenge, data=dict)
+
+    while True:
+        _request = requests.post(server_challenge, data=dict)
         _request = _request.json()
-        
+
         if 'finnish' in _request:
             server_done = _request['done_url']
             return _request['finnish'], _request['token_tmp']
-        
+
         recieved_challenge = _request['challenge']
         bit = _request['bit']
         message = password + str(challenge) + str(recieved_challenge)
@@ -389,21 +420,24 @@ def calculate_challenge(user, challenge, token_tmp):
         digest.update(message.encode())
         bits = BitArray(hex=digest.hexdigest())
         bit_to_send = bits.bin[0]
-        
+
         next_challenge = randint(0, 100)
         challenge = next_challenge
-        
+
         if int(bit) == int(bit_to_send) and valores_errados == False:
             message = password + str(recieved_challenge) + str(next_challenge)
             digest = hashlib.sha3_512()
             digest.update(message.encode())
             bits = BitArray(hex=digest.hexdigest())
             bit_to_send = bits.bin[0]
-            dict = {"challenge": next_challenge, "bit": bit_to_send, 'token_tmp': token_tmp }
+            dict = {"challenge": next_challenge,
+                    "bit": bit_to_send, 'token_tmp': token_tmp}
         else:
             valores_errados = True
             bit_to_send = randint(0, 1)
-            dict = {"challenge": next_challenge, "bit": bit_to_send, 'token_tmp': token_tmp }
+            dict = {"challenge": next_challenge,
+                    "bit": bit_to_send, 'token_tmp': token_tmp}
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=9874)
