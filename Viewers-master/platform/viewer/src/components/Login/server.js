88765aef7c4fa559e249b40a841d40c168c82de8
TheScorpoi <sobral@ua.pt>
2022-05-15 02:55:18 +0100

    add files to mysql database

--- Viewers-master/platform/viewer/src/components/Login/server.js
+++ Viewers-master/platform/viewer/src/components/Login/server.js
@@ -7,16 +7,20 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'root',
     database: 'mi4web'
 });
 
 app.get("/testar_db", (req, res) => {
+    connection.connect();
+    console.log("lalalalalalalalal");
     connection.query('SELECT * FROM users', (err, rows, fields) => {
         if (err) {
+            console.log("DEU TUDO ERRADO");
             console.log(err);
             //res.sendStatus(500);
         } else {
+            console.log("DEU CERTO");
             console.log(rows);
             //res.send(rows);
         }
