c16821990d55e84d8c81ea4c2c7e4886a12dde5f
daniff15 <dani.fig@ua.pt>
2022-05-20 02:20:14 +0100

    chart e delete unnesed stuff

--- Viewers-master/platform/viewer/src/components/Admin/Dashboard/pages/Chart.jsx
+++ Viewers-master/platform/viewer/src/components/Admin/Dashboard/pages/Chart.jsx
@@ -1,11 +1,60 @@
 import React from 'react';
 import './chart.css';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => [100, 200, 300, 302, 4, 543, 234]),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ],
+};
 
 export default function Chart() {
   return (
     <div className="chart">
-        <div className="chartItem">
-        </div>
+      <div className="chartItem">
+        <Line options={options} data={data} />;
+      </div>
     </div>
   );
 }
