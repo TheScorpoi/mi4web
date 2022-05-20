c05656fc25b3fd3bf774098e13819f74f9371375
MartaFradique <martafradique@ua.pt>
2022-05-20 16:32:48 +0100

    opah ya

--- Viewers-master/platform/viewer/src/components/Admin/Dashboard/pages/InitialDashboard.js
+++ Viewers-master/platform/viewer/src/components/Admin/Dashboard/pages/InitialDashboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './InitialDashboard.css';
-import Dash from './Dash';
-import Chart from './Chart';
-import Dash2 from './Dash2';
+import Dash from './Dashboard/Dash';
+import Chart from './Dashboard/Chart/Chart';
+import Dash2 from './Dashboard/Dash2';
 
 function InitialDashboard() {
   return (
     <div className="initialDashboard">
+      
       <Dash2 />
       <Dash />
       <Chart />
