38e6abbc7956798e869da052a466de3062815620
daniff15 <dani.fig@ua.pt>
2022-05-20 01:30:29 +0100

    A marta vai ficar fodida, mas tou me a cagar

--- Viewers-master/platform/viewer/src/components/Admin/Dashboard/pages/Dash.jsx
+++ Viewers-master/platform/viewer/src/components/Admin/Dashboard/pages/Dash.jsx
@@ -4,58 +4,57 @@ import PeopleIcon from '@material-ui/icons/People';
 import api from './api';
 
 export default function Dash() {
-
   const [data_stats, setData] = React.useState([]);
   const [data_system, setDataSystem] = React.useState([]);
 
-  console.log("aqui oh cornos");
+  console.log('aqui oh cornos');
 
   React.useEffect(() => {
     api.get('/statistics').then(res => {
       setData(res.data);
-      console.log(res)
+      console.log(res);
     });
   }, []);
 
   React.useEffect(() => {
     api.get('/system').then(res => {
       setDataSystem(res.data);
-      console.log(res)
+      console.log(res);
     });
   }, []);
-  
+
   return (
     <div className="dash">
       <div className="dashItem">
         <span className="dashTitle">Count Instances</span>
         <div className="dashInstancesContainer">
-          <span className="dashInstances">{ data_stats.CountInstances}</span>
-          
+          <span className="dashInstances">{data_stats.CountInstances}</span>
         </div>
       </div>
-      
+
       <div className="dashItem">
         <span className="dashTitle">Count Patients</span>
         <div className="dashInstancesContainer">
-          <span className="dashInstances">{ data_stats.CountPatients}</span>
+          <span className="dashInstances">{data_stats.CountPatients}</span>
         </div>
       </div>
 
       <div className="dashItem">
         <span className="dashTitle">Count Series</span>
         <div className="dashInstancesContainer">
-          <span className="dashInstances"> { data_stats.CountSeries}</span>
-          <div className="dashIcons"><PeopleIcon size={70}/></div>
+          <span className="dashInstances"> {data_stats.CountSeries}</span>
+          <div className="dashIcons">
+            <PeopleIcon size={70} />
+          </div>
         </div>
       </div>
 
       <div className="dashItem">
         <span className="dashTitle">Total Disk Size</span>
         <div className="dashInstancesContainer">
-          <span className="dashInstances">{ data_stats.TotalDiskSizeMB}</span>
+          <span className="dashInstances">{data_stats.TotalDiskSizeMB}</span>
         </div>
       </div>
-      </div>
-    
+    </div>
   );
 }
