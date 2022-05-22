6760f1f2bf12bc0cb89c88c2f9060ee7a7b42096
MartaFradique <martafradique@ua.pt>
2022-05-22 05:09:02 +0100

    nodes css and structure completed

--- Viewers-master/platform/viewer/src/components/Admin/Dashboard/Nodes.js
+++ Viewers-master/platform/viewer/src/components/Admin/Dashboard/Nodes.js
@@ -2,34 +2,52 @@ import CytoscapeComponent from "react-cytoscapejs";
 import Cytoscape from "cytoscape";
 import COSEBilkent from "cytoscape-cose-bilkent";
 import React from 'react';
+import './nodes.css';
 
 Cytoscape.use(COSEBilkent);
 export default function App() {
   const layout = { name: "cose-bilkent" };
   return (
-    <div style={{ display: "flex", flex: 1 }}>
-      <CytoscapeComponent
-        
-        zoom={2}
-        zoomingEnabled={false}
-        layout={layout}
-        elements={CytoscapeComponent.normalizeElements({
-          nodes: [
-            { data: { id: "one", label: "Node 1" }, position: { x: 0, y: 0 } },
-            { data: { id: "two", label: "Node 2" }, position: { x: 100, y: 0 } }
-          ],
-          edges: [
-            {
-              data: {
-                source: "one",
-                target: "two",
-                label: "Edge from Node1 to Node2"
-              }
-            }
-          ]
-        })}
-        style={{ width: "100%", height: "500px", marginTop: "100px" }}
-      />
+    <><div>
+      <span className="titleDashboard">DICOM Nodes</span>
     </div>
+    <div className="nodes">
+        <div className="nodesItem">
+          <div style={{ display: "flex", flex: 3 }}>
+            <CytoscapeComponent
+
+              zoom={2}
+              zoomingEnabled={false}
+              layout={layout}
+              elements={CytoscapeComponent.normalizeElements({
+                nodes: [
+                  { data: { id: "one", label: "Putas1" }, position: { x: 0, y: 0 }, style: { color: "SkyBlue" } },
+                  { data: { id: "two", label: "E " }, position: { x: 100, y: 0 }, style: { color: "SkyBlue" } },
+                  { data: { id: "three", label: "VINHO VERDE" }, position: { x: 100, y: 0 }, style: { color: "SkyBlue" } },
+                  
+                ],
+                edges: [
+                  {
+                    data: {
+                      source: "one",
+                      target: "two",
+                      label: "Edge from Node1 to Node2"
+                    }
+                  },
+                  {
+                    data: {
+                      source: "two",
+                      target: "three",
+                      label: "Edge from Node2 to Node3"
+                    }
+                  },
+                ],
+              })}
+              style={{ width: "100%", height: "500px", marginTop: "0px", color: "white" }} />
+          </div>
+        </div>
+
+      </div></>
+    
   );
 }
--- Viewers-master/platform/viewer/src/components/Admin/Dashboard/pages/Dashboard/Dash.jsx
+++ Viewers-master/platform/viewer/src/components/Admin/Dashboard/pages/Dashboard/Dash.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './dash.css';
 import PeopleIcon from '@material-ui/icons/People';
+import Filter9PlusIcon from '@material-ui/icons/Filter9Plus';
+import ViewModuleIcon from '@material-ui/icons/ViewModule';
+import SaveIcon from '@material-ui/icons//Save';
 import api from '../api';
 
 export default function Dash() {
@@ -15,36 +18,55 @@ export default function Dash() {
 
   return (
     <div className="dash">
-      <div className="dashItem">
-        <span className="dashTitle">Count Instances</span>
-        <div className="dashInstancesContainer">
-          <span className="dashInstances">{data_stats.CountInstances}</span>
-        </div>
-      </div>
 
       <div className="dashItem">
-        <span className="dashTitle">Count Patients</span>
-        <div className="dashInstancesContainer">
-          <span className="dashInstances">{data_stats.CountPatients}</span>
-        </div>
-      </div>
-
-      <div className="dashItem">
-        <span className="dashTitle">Count Series</span>
-        <div className="dashInstancesContainer">
-          <span className="dashInstances"> {data_stats.CountSeries}</span>
-          <div className="dashIcons">
-            <PeopleIcon size={70} />
+        <div className="part1">
+          <span className="dashTitle">Count Instances</span>
+          <div className="dashInstancesContainer">
+            <span className="dashInstances">{data_stats.CountInstances}</span>
           </div>
         </div>
+        <div className="dashIcons">
+            <Filter9PlusIcon  fontSize="large" />
+        </div>
       </div>
 
       <div className="dashItem">
-        <span className="dashTitle">Total Disk Size</span>
-        <div className="dashInstancesContainer">
-          <span className="dashInstances">{data_stats.TotalDiskSizeMB}</span>
+        <div className="part1">
+          <span className="dashTitle">Count Patients</span>
+          <div className="dashInstancesContainer">
+            <span className="dashInstances">{data_stats.CountPatients}</span>
+          </div>
+        </div>
+        <div className="dashIcons">
+            <PeopleIcon  fontSize="large" />
         </div>
       </div>
+
+      <div className="dashItem">
+        <div className="part1">
+          <span className="dashTitle">Count Series</span>
+          <div className="dashInstancesContainer">
+            <span className="dashInstances"> {data_stats.CountSeries}</span>
+          </div>
+        </div>
+        <div className="dashIcons">
+            <ViewModuleIcon  fontSize="large" />
+        </div>
+      </div>
+
+      <div className="dashItem">
+        <div className="part1">
+          <span className="dashTitle">Total Disk Size</span>
+          <div className="dashInstancesContainer">
+            <span className="dashInstances">{data_stats.TotalDiskSizeMB}</span>
+          </div>
+        </div>
+        <div className="dashIcons">
+            <SaveIcon  fontSize="large" />
+        </div>
+      </div>
+
     </div>
   );
 }
