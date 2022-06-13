fa3dd51dc594469cde414d9d665504ab03baca7f
daniff15 <dani.fig@ua.pt>
2022-06-13 16:59:37 +0100

    sing in coiso

--- Viewers-master/platform/viewer/src/components/Header/Header.js
+++ Viewers-master/platform/viewer/src/components/Header/Header.js
@@ -13,7 +13,6 @@ import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 import api from '../Admin/Dashboard/pages/ApiConnections/apiManageAccess';
 import { SignUp } from '../Login/SignUp';
 
-
 function Header(props) {
   const {
     t,
@@ -41,6 +40,7 @@ function Header(props) {
     React.useEffect(() => {
       api.get(`/get_user_from_token/${id}`).then(res => {
         setData(res.data);
+        console.log('SOU A DATA MEU FDP');
         console.log(res.data);
       });
     }, []);
@@ -83,7 +83,11 @@ function Header(props) {
   if (localStorage.getItem('user') == null) {
     signIn.current = false;
   } else {
-    signIn.current = true;
+    if (localStorage.getItem('user') == '[]') {
+      signIn.current = false;
+    } else {
+      signIn.current = true;
+    }
   }
 
   function putNameOnHeader() {
@@ -128,16 +132,28 @@ function Header(props) {
           </div>
 
           {!signIn.current ? (
-            <button
-              className="button-3"
-              onClick={e => {
-                e.preventDefault();
-                window.location.href = 'http://localhost:9874/show_login';
-              }}
-            >
-              Login
-            </button>
-            
+            <>
+              <button
+                className="button-3"
+                onClick={e => {
+                  e.preventDefault();
+                  window.location.href = 'http://localhost:9874/show_login';
+                }}
+              >
+                Login
+              </button>
+              <button
+                className="button-3"
+                onClick={() =>
+                  show({
+                    content: SignUp,
+                    title: t('Regist account'),
+                  })
+                }
+              >
+                SignUp
+              </button>
+            </>
           ) : (
             <button
               className="button-3"
@@ -150,20 +166,7 @@ function Header(props) {
             >
               Logout
             </button>
-          )
-          }
-           <button
-            className="button-3"
-            onClick={() =>
-              show({
-                content: SignUp,
-                title: t('Regist account'),
-              })
-            }
-          >
-            SignUp
-          </button>
-          
+          )}
         </div>
       </div>
     </>
