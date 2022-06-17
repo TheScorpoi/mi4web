0aeb7003a0c572474886c153553160fe3def10a1
daniff15 <dani.fig@ua.pt>
2022-06-17 16:25:53 +0100

    update use case diagram text

--- project-website/src/pages/LandingPages/Documentation/sections/data/imageBlockData.js
+++ project-website/src/pages/LandingPages/Documentation/sections/data/imageBlockData.js
@@ -26,7 +26,7 @@ export default [
     image: usecaseadmin,
     image2: usecaserest,
     lildescription:
-      "Firstly, we created the table user, which has two descendents, the table not_accepted, which are the users that are yet to be accepted by the administrator, and the table staff, which are the users that were already verified and accepted by the administrator. There is also the table admin, which defines the administrator of our application, having the responsability of checking and accepting users, and manage everything. Lastly, we also have the table chart_info, which provides the information about the number of studies carried out per day.",
+      "We have four main actors in the system. The system manager, which is an administrator of the system, and is able to manage the access to the application. The guest which is a non-logged user, has the same funcionalities has a Referring Imaging Staff, although he only has acess to a certain amount of fictitious studies. The Clinical Imaging Staff, have acess to the Referring Imaging Staff functionalities and can make reports and make annotations on the pacient's studies.",
   },
   {
     title: "Information Model",
